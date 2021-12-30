0512214beb85c52da78c48df79db5075b0a9d60e
lihui <mengwei182@outlook.com>
2021-12-30 14:29:43 +0800

    提交文件

--- src/main/java/org/example/security/SecurityConfiguration.java
+++ src/main/java/org/example/security/SecurityConfiguration.java
@@ -34,7 +34,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/**/*.js", "/**/*.png", "/**/*.ico");
+        web.ignoring().antMatchers(antMatchers.split(","));
     }
 
     @Override
